4302f2ad40f451de307258e469d3e3d4c4dd7cb2
Janaina Santos <janaina.santos.dev@gmail.com>
2022-01-17 08:48:44 -0300

    Commit Inicial

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Meu primeiro site em PHP!Woohoo!</title>
+</head>
+<body>
+    <?php
+     for($i = 0; $i < 10; $i++){
+      print("Linha numero "; $i, "</br>");   
+     }
+    ?> 
+</body>
+</html>
